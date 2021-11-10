c40ce15abc5d302d36f0673a31e783177edb6e04
Leonid Khlebushchev <lowpold1@gmail.com>
2021-11-10 09:31:12 +0300

    V0.1.1 Task:31642144 Fixes for LPT communication

--- cpanel.py
+++ cpanel.py
@@ -8,12 +8,12 @@ import time
 
 class CPanel:
     LPT_Map = {
-                'STB' : 1,
+#                'STB' : 1,
                 'POWER' : 3,
                 'RUNNING' : 5,
                 'FAILED' : 7,
                 'OK' : 9,
-                'ERROR' : 4
+                'START' : 4
             }
 
     #blink_busy = []
@@ -24,12 +24,13 @@ class CPanel:
 
         try:
             self.__port = parallel.Parallel()
-            self.__writeBit(CPanel.LPT_Map['STB'], 0)
+#            self.__writeBit(CPanel.LPT_Map['STB'], 0)
+            self.__port.setDataStrobe(0)
         except Exception as e:
             print(e)
 
     def LEDSet(self, ledname, state):
-        if ledname.upper() == 'STB' or ledname.upper() == 'ERROR':
+        if ledname.upper() == 'STB' or ledname.upper() == 'START':
             print('Bad name for LED (' + ledname + ')')
             return
 
@@ -45,35 +46,35 @@ class CPanel:
 
         if state.upper() == 'ON':
             self.__writeBit(CPanel.LPT_Map[ledname], 1)
-            #strobe write
-            self.__writeBit(CPanel.LPT_Map['STB'], 1)
-            time.sleep(0.1)
-            self.__writeBit(CPanel.LPT_Map['STB'], 0)
         elif state.upper() == 'OFF':
             self.__writeBit(CPanel.LPT_Map[ledname], 0)
-            #strobe write
-            self.__writeBit(CPanel.LPT_Map['STB'], 1)
-            time.sleep(0.1)
-            self.__writeBit(CPanel.LPT_Map['STB'], 0)
         elif state.upper() == 'BLINK':
             print('That state is not working yet')
 
     def isPushed(self):
         if self.__port:
-            value = self.__port.getData() & (1 << CPanel.LPT_Map['ERROR'])
+            value = self.__port.getInError()
 
             if value:
                 return True
 
         return False
 
-    def __writeBit(self, bitnum, value):
+    def __writeBit(self, ledname, value):
         if self.__port:
-            bit = 1 << bitnum
+            #convert index to setDataValue
+            i = list(CPanel.LPT_Map.keys()).index(ledname)
+            index = 1 << (CPanel.LPT_Map[ledname] - 3 - i)
 
             if value == 1:
-                self.__port.setData(self.__port.data() | bit)
+                self.__port.setData(index)
+                self.__port.setDataStrobe(1)
+                time.sleep(0.1)
+                self.__port.setDataStrobe(0)
             elif value == 0:
-                self.__port.setData(self.__port.data() & (255 - bit))
+                self.__port.setData(index & (255 - index))
+                self.__port.setDataStrobe(1)
+                time.sleep(0.1)
+                self.__port.setDataStrobe(0)
             else:
                 print('Bad value for setting bit')
